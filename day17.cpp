f8b2b2c563ea3c11c0772fe9167bbf2a1f2482b4
jpcerrone <jpcerronex@gmail.com>
2022-12-27 12:36:32 -0300

    day 17 part 1

---
+++ day17.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "utils/coordinate.h"
+
+#define LOG(x) std::cout << x << std::endl;
+#define Matrix std::vector<std::vector<bool>>
+
+Matrix grid; // A stack of rows, starting from the bottom
+const int GRID_WIDTH = 7;
+
+const int NUMBER_OF_PIECES = 5;
+std::vector<Matrix> pieces = {
+    {{1,1,1,1}},
+    {
+        {0,1,0},
+        {1,1,1},
+        {0,1,0}
+    },
+    { // Inverted for easier calculations, bottom up
+        {1,1,1},
+        {0,0,1},
+        {0,0,1}
+    },
+        {
+        {1},
+        {1},
+        {1},
+        {1}
+    },
+        {
+        {1,1},
+        {1,1},
+    }   
+};
+
+
+
+const std::string inputFilePath = "day17.txt";
+
+bool canPieceMoveDown(Matrix* currentPiece, Coordinate position){
+    const std::vector<bool> pieceLastRow = currentPiece->at(0);
+    for(int x=0; x < pieceLastRow.size(); x++){
+        int upperMostY = 0;
+        for(int y=0; y < currentPiece->size(); y++){
+            if (!currentPiece->at(y)[x]){
+                upperMostY +=1;
+            }else{
+                break;
+            }
+        }
+        if (currentPiece->at(upperMostY)[x] && grid[position.y + upperMostY -1][x + position.x]){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool canPieceMoveLeft(Matrix* currentPiece, Coordinate position){
+    if (position.x == 0){
+        return false;
+    }
+    for (int y = 0; y < currentPiece->size(); y++){
+        int leftMostX = 0;
+        for(int x=0; x < currentPiece->at(y).size(); x++){
+            if (!currentPiece->at(y)[x]){
+                leftMostX +=1;
+            }else{
+                break;
+            }
+        }
+        if (currentPiece->at(y)[leftMostX] && grid[position.y + y][position.x + leftMostX- 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool canPieceMoveRight(Matrix* currentPiece, Coordinate position){
+    const std::vector<bool> pieceLastRow = currentPiece->at(currentPiece->size()-1);
+    if (position.x + pieceLastRow.size()> GRID_WIDTH -1){
+        return false;
+    }
+    for (int y = 0; y < currentPiece->size(); y++){
+        int rightMostX = pieceLastRow.size() - 1;
+        for(int x=currentPiece->at(y).size() - 1; x >= 0; x--){
+            if (!currentPiece->at(y)[x]){
+                rightMostX -=1;
+            }
+            else{
+                break;
+            }
+        }
+        if (currentPiece->at(y)[rightMostX] && grid[position.y + y][position.x +rightMostX + 1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+void placePiece(Matrix* currentPiece, Coordinate position){
+    for(int y=0; y < currentPiece->size(); y++){
+        for(int x = 0; x < currentPiece->at(0).size(); x++){
+            if (currentPiece->at(y)[x])
+            grid[y+position.y][x+position.x] = currentPiece->at(y)[x];
+        }
+    }
+}
+
+void printGrid(Matrix* currentPiece, Coordinate position){
+    for(int y = 30; y >= 0; y--){
+        for (int x = 0; x < GRID_WIDTH ; x++){
+            Coordinate current = {x,y};
+            if (current.inRange({position.x, position.y}, {position.x + (int)currentPiece->at(0).size() -1, position.y + (int)currentPiece->size() - 1 })){
+                std::cout << "@";
+            }
+            else
+            std::cout << grid[y][x];
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main(){
+    Coordinate currentPiecePos = {2, 4}; //Starting from the bottom left
+
+    std::vector<bool> floor = {1,1,1,1,1,1,1};
+    LOG(sizeof(floor));
+    grid.push_back(floor); // Floor
+
+    for(int i=0; i< 20000; i++){
+        grid.push_back({0,0,0,0,0,0,0});
+    }
+    long rocksThatStopped = 0;
+    long yForHighestRockInRoom = 0;
+
+    // Test
+    # if 0
+    Coordinate piece2C = {3,1};
+    Matrix piece2 = pieces[2];
+    placePiece(&pieces[1],{1,1});
+    printGrid(&piece2, piece2C);
+    LOG(canPieceMoveLeft(&piece2, piece2C));
+    LOG(canPieceMoveRight(&piece2, piece2C));
+    LOG(canPieceMoveDown(&piece2, piece2C));
+
+    # endif
+    std::ifstream ifs(inputFilePath, std::ifstream::in);
+    std::string input;
+    std::getline(ifs, input);
+    LOG(input.size());
+    std::string::iterator it = input.begin(); // Hack to not perform i++ on the first iteration
+    while(rocksThatStopped < 2022){ 
+        if (it==input.end()){ // Repeat input if it has been fully consumed
+            it = input.begin();
+        }
+        Matrix* currentPiece = &pieces[rocksThatStopped % NUMBER_OF_PIECES];
+        //printGrid(currentPiece, currentPiecePos);
+        if ((*it == '<') && canPieceMoveLeft(currentPiece, currentPiecePos)){
+            currentPiecePos.x -= 1;
+        }
+        else if ((*it == '>') && canPieceMoveRight(currentPiece, currentPiecePos)){
+            currentPiecePos.x += 1;
+        }
+        if(canPieceMoveDown(currentPiece, currentPiecePos)){
+            currentPiecePos.y -= 1;
+        }else{
+            placePiece(currentPiece, currentPiecePos);
+            yForHighestRockInRoom = std::max(yForHighestRockInRoom, (long)(currentPiecePos.y + currentPiece->size() -1));
+            rocksThatStopped += 1;
+            currentPiecePos = {2, yForHighestRockInRoom + 4};
+        }
+        it++;
+    }
+
+    std::cout << "--Ex1 Output: " << yForHighestRockInRoom << std::endl;
+    std::cout << "--Ex2 Output: " << "" << std::endl;
+
+    return 0;
+}
